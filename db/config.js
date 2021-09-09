c596111c9b1767f09c04da01b800c59c06c5e95f
CiccioTOP <francesco.sillitti@community.unipa.it>
2021-09-09 17:54:42 +0200

    config

--- db/config.js
+++ db/config.js
@@ -2,10 +2,10 @@
 //Esenpio w3s connessione db con comando 'node <<nome_file.esetnsione>>'
 exports.config = {
   connectionLimit: 10,
-  host: 'localhost',
+  host: 'dariobirtone.mysql.database.azure.com',
   // Non usiamo *** mai *** root senza password
-  user: 'dario',
-  password: 'dario123',
+  user: 'DarioBirtone@dariobirtone',
+  password: 'Dario123',
   database: 'provadb',
   multipleStatements: true // consente query multiple in un'unica istruzione SQL
  
